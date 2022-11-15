8f9197cfd6be24a66fcb6c2b19813f744fec181f
lucrhode <luc.rhode@ontariotechu.net>
2022-11-15 14:52:01 -0500

    Player Movement
    - added Collider2D component to player object
    - added forward/backward movement, and left/right rotation to player object
    - added restraints to player movement, set bounds to within camera area

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerMoveSpeed = 8.0f;
+    public float playerRotateSpeed = 300.0f;
+    float vAxisInput;
+    float hAxisInput;
+
+    Camera cam;
+    Collider2D objCollider;
+
+    void Start()
+    {
+        cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        objCollider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        vAxisInput = Input.GetAxis("Vertical") * playerMoveSpeed;
+
+        // Inverted horizontal (A-D) axis for rotation
+        hAxisInput = -1 * (Input.GetAxis("Horizontal")) * playerRotateSpeed;
+
+        // Move player along the "Forard-Backward" (local Y-Axis)
+        this.transform.Translate(Vector2.up * vAxisInput * Time.deltaTime);
+
+        // Find player location relative to camera
+        Vector3 playerInCamPos = cam.WorldToViewportPoint(this.transform.position);
+
+        // Find the size of the objects collision box (convert to screen space units)
+        Vector3 spriteSize = cam.WorldToViewportPoint(objCollider.bounds.size);
+
+        /* Restricts movement values to the screen space
+         * (0, 0) to (1, 1)
+         * Uses the collision box to calculate object height/width
+         */
+        playerInCamPos.x = Mathf.Clamp(playerInCamPos.x, 0 + (spriteSize.x / 17), 1 - (spriteSize.x / 17));
+        playerInCamPos.y = Mathf.Clamp(playerInCamPos.y, 0 + (spriteSize.y / 10), 1 - (spriteSize.y / 10));
+        this.transform.position = cam.ViewportToWorldPoint(playerInCamPos);
+
+        // Rotate the player character along the Z-Axis
+        this.transform.Rotate(Vector3.forward * hAxisInput * Time.deltaTime);
+    }
+}
